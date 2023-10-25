f5bcdf18c9aa0d04d04d34085583bedddecb97bd
Dev <usamachitoo@gmail.com>
2023-10-25 15:56:36 -0400

    comparePage Updated

--- src/pages/comparePokemonPage.js
+++ src/pages/comparePokemonPage.js
@@ -2,16 +2,26 @@ import React, { useState } from "react";
 import Loader from "../component/Loader";
 import { useFetchPokemonByName } from "../hooks/useFetchPokemonByName";
 import PokemonDetail from "../component/PokemonDetail";
+import debounce from "lodash/debounce";
 
 function ComparePokemonPage() {
-  const [pokemonName1, setPokemonName1] = useState("");
-  const [pokemonName2, setPokemonName2] = useState("");
+  const [debouncedName1, setDebouncedName1] = useState("");
+  const [debouncedName2, setDebouncedName2] = useState("");
+  console.log(debouncedName1);
 
   const { data: pokemonData1, isLoading: isLoadingData1 } =
-    useFetchPokemonByName(pokemonName1);
+    useFetchPokemonByName(debouncedName1);
 
   const { data: pokemonData2, isLoading: isLoadingData2 } =
-    useFetchPokemonByName(pokemonName2);
+    useFetchPokemonByName(debouncedName2);
+
+  const debouncedSearch1 = debounce((e) => {
+    setDebouncedName1(e.target.value);
+  }, 3000);
+
+  const debouncedSearch2 = debounce((e) => {
+    setDebouncedName2(e.target.value);
+  }, 3000);
 
   return (
     <div>
@@ -22,16 +32,15 @@ function ComparePokemonPage() {
             className="w-[200px] h-10 px-4    mb-[10px] md:ms-0 lg:mb-0 text-gray-800 border-2 border-gray-300 rounded-md self-start lg:self-center   focus:outline-none"
             type="text"
             placeholder="Search Name"
-            value={pokemonName1}
-            onChange={(e) => setPokemonName1(e.target.value.toLowerCase())}
+            onChange={debouncedSearch1}
           />
-          {pokemonName2 !== "" && isLoadingData1 ? (
+          {debouncedName2 !== "" && isLoadingData1 ? (
             <Loader />
           ) : pokemonData1?.base_experience ? (
-            pokemonName1 !== "" ? (
+            debouncedName1 !== "" ? (
               <PokemonDetail pokemonDetail={pokemonData1} />
             ) : null
-          ) : pokemonName1 !== "" && pokemonData1?.base_experience !== "" ? (
+          ) : debouncedName1 !== "" && pokemonData1?.base_experience !== "" ? (
             <span className="block font-bold my-5">Pokemon not found</span>
           ) : null}
         </div>
@@ -41,17 +50,16 @@ function ComparePokemonPage() {
             className="w-[200px] h-10 px-4 py-2  md:ms-0   text-gray-800 border-2 border-gray-300  rounded-md self-start lg:self-center  focus:outline-none "
             type="text"
             placeholder="Search Name"
-            value={pokemonName2}
-            onChange={(e) => setPokemonName2(e.target.value.toLowerCase())}
+            onChange={debouncedSearch2}
           />
 
-          {pokemonName2 !== "" && isLoadingData2 ? (
+          {debouncedName2 !== "" && isLoadingData2 ? (
             <Loader />
           ) : pokemonData2?.base_experience ? (
-            pokemonName2 !== "" ? (
+            debouncedName2 !== "" ? (
               <PokemonDetail pokemonDetail={pokemonData2} />
             ) : null
-          ) : pokemonName2 !== "" && pokemonData2?.base_experience !== "" ? (
+          ) : debouncedName2 !== "" && pokemonData2?.base_experience !== "" ? (
             <span className="block font-bold my-5">Pokemon not found</span>
           ) : null}
         </div>
